b0ee107a1c5238663f6b375fcc94795948091194
rayechen <77929622+rayechen@users.noreply.github.com>
2021-07-02 02:57:38 +0800

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -7,15 +7,17 @@ def test_x():
   i = 1
   assert 1 == i*2/2
 
-def test_ppl():
+def build_ppl():
   exec_ok = ppltest.ppl_build_with_jenkins()
   assert exec_ok
   
+def test_ppl_x86():
   result = ppltest.ppltest_x86_with_jenkins()
   assert result['exec_ok']
   assert 'SUCCESS' == result['testcases_result']['result_status']
   assert 0 == result['testcases_result']['failed']
   
+def test_ppl_cuda():
   result = ppltest.ppltest_cuda_with_jenkins()
   assert result['exec_ok']
   assert 'SUCCESS' == result['testcases_result']['result_status']
