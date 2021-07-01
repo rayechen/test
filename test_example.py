74abb742900b4089c81806a57998f1255905bf5e
rayechen <77929622+rayechen@users.noreply.github.com>
2021-07-01 19:43:22 +0800

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,7 +1,14 @@
 import os
 import sys
 import pytest
+import ppltest
 
 def test_x():
   i = 1
   assert 1 == i*2/2
+
+def test_ppl():
+  result = ppltest.ppltest_with_jenkins()
+  assert result['exec_ok']
+  assert 0 == result['testcase_result']['failed']
+  
