2e63e1ad350ebb1c24c467bd903e2a3d6c77debb
rayechen <77929622+rayechen@users.noreply.github.com>
2021-07-05 23:42:26 +0800

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -1,18 +1,22 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='manual to this script')
-parser.add_argument('--github_run_id', type=str, default = None)
-parser.add_argument('--github_run_number', type=str, default=None)
-parser.add_argument('--sha', type=str, default=None)
-args = parser.parse_args()
-print(args.github_run_id, type(args.github_run_id))
-print(args.github_run_number, type(args.github_run_number))
-print(args.sha, type(args.sha))
+def print_params():
+  parser = argparse.ArgumentParser(description='manual to this script')
+  parser.add_argument('--github_run_id', type=str, default = None)
+  parser.add_argument('--github_run_number', type=str, default=None)
+  parser.add_argument('--sha', type=str, default=None)
+  args = parser.parse_args()
+  print(args.github_run_id, type(args.github_run_id))
+  print(args.github_run_number, type(args.github_run_number))
+  print(args.sha, type(args.sha))
 
 # print('sleeping 50s...')
 # import time
 # time.sleep(50)
-import sys
-sys.exit(-1)
+# import sys
+# sys.exit(-1)
 
+if __name__ == "__main__":
+  print_params()
+  return -1
